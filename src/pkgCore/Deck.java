ca123de35e672d4c251a95d6331eab9bd170dded
chackolate0 <36235186+chackolate0@users.noreply.github.com>
2018-03-13 19:40:20 -0400

    try 1

---
+++ src/pkgCore/Deck.java
@@ -0,0 +1,59 @@
+package pkgCore;
+
+
+import java.util.*;
+import pkgEnum.*;
+import java.lang.Math.*;
+
+
+public class Deck {
+	
+	Random rand = new Random();
+	
+	public ArrayList<Card> Cards = new ArrayList<Card>();
+
+	public Deck(int numDecks) {
+		super();
+		for (int i = 1; i <= numDecks; i++) {
+				for (int j = 1; j <= eSuit.values().length; j++) {
+						for (int k = 1; k <= eRank.values().length; k++) {
+								Card card = new Card(eSuit.values()[j-1],eRank.values()[k]);
+								this.Cards.add(card);
+						}
+				}
+		}
+		Collections.shuffle(Cards);
+	}
+	
+	public Deck() {
+		this(1);
+	}
+	
+	public Card draw() throws Exception {
+		if (Cards.size() == 0) {
+			throw new Exception("Deck is empty");
+		}
+		Card drawnCard = this.Cards.get(0);
+		Cards.remove(drawnCard);
+		return drawnCard;
+	}
+	
+	public int getRemaining(Object eNum)
+	{
+		int count = 0;
+		if(eNum instanceof eRank){
+			for(Card c : Cards){
+				if(c.geteRank() == eNum)
+					count++;
+			}
+		}
+		else{
+			for(Card c : Cards){
+				if(c.geteSuit() == eNum) {
+					count++;
+				}
+			}
+		}
+		return count;		
+	}
+}
